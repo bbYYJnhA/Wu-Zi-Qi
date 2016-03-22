1b7606d2228116e198b2b0a92c8c59b52dfd72c6
markbaltic <mark.baltic@gmail.com>
2016-03-22 21:24:24 +0100

    Popravil sosledje v vrednost_boljsa.

--- vrednost_boljsa.py
+++ vrednost_boljsa.py
@@ -6,7 +6,7 @@ BELI = 2
 
 seznam_1 = ["01110", "0110", "010", "211101", "011010", "010110"]
 seznam_2 = ["211110", "011112", "101112", "211011", "110112", "21110", "01112", "2110", "0112", "210", "012"]
-def crni(niz, na_potezi):
+def crni(niz, barva):
 	#prvi in zanji element, da se ujema z elementi iz seznama
 	niz = "2" + niz + "2"
 	vrednost = 0
@@ -15,11 +15,11 @@ def crni(niz, na_potezi):
 		return vrednost
 	elif niz.count("011110") > 0:
 		vrednost += ZMAGA//2
-	elif niz.count("01110") > 0 and na_potezi == BELI:
+	elif niz.count("01110") > 0 and barva == BELI:
 		vrednost += ZMAGA//2
-	elif niz.count("11110") > 0 and na_potezi == BELI:
+	elif niz.count("11110") > 0 and barva == BELI:
 		vrednost += ZMAGA//2
-	elif niz.count("01111") > 0 and na_potezi == BELI:
+	elif niz.count("01111") > 0 and barva == BELI:
 		vrednost += ZMAGA//2
 	for el in seznam_1:
 		dolzina = len(el)
@@ -38,7 +38,7 @@ def crni(niz, na_potezi):
 seznam_3 = ["02220", "0220", "020", "122202", "022020", "020220"]
 seznam_4 = ["122220", "022221", "202221", "122022", "220221" "12220", "02221", "1220", "0221", "120", "021"]
 
-def beli(niz, na_potezi):
+def beli(niz, barva):
 	niz = "1" + niz + "1"
 	vrednost = 0
 	if niz.count("22222") > 0:
@@ -46,11 +46,11 @@ def beli(niz, na_potezi):
 		return vrednost
 	elif niz.count("022220") > 0:
 		vrednost += ZMAGA//2
-	elif niz.count("02220") > 0 and na_potezi == CRNI:
+	elif niz.count("02220") > 0 and barva == CRNI:
 		vrednost += ZMAGA//2
-	elif niz.count("22220") > 0 and na_potezi == CRNI:
+	elif niz.count("22220") > 0 and barva == CRNI:
 		vrednost += ZMAGA//2
-	elif niz.count("02222") > 0 and na_potezi == CRNI:
+	elif niz.count("02222") > 0 and barva == CRNI:
 		vrednost += ZMAGA//2
 	for el in seznam_4:
 		dolzina = len(el)
@@ -65,19 +65,19 @@ def beli(niz, na_potezi):
 
 	return vrednost
 
-def vrednost_vrstic(tabela, na_potezi):
+def vrednost_vrstic(tabela, barva):
 	rezultat = 0
 	for vrstica in tabela:
 		nizek = ""
 		for znak in vrstica:
 			nizek += str(znak)
-		rezultat += crni(nizek, na_potezi) - beli(nizek, na_potezi)
+		rezultat += crni(nizek, barva) - beli(nizek, barva)
 	return rezultat
 
-def vrednost_stolpcev(tabela, na_potezi):
+def vrednost_stolpcev(tabela, barva):
 	#trans = numpy.array(tabela).transpose()
 	stolpci = [list(x) for x in zip(*tabela)]
-	return vrednost_vrstic(stolpci, na_potezi)
+	return vrednost_vrstic(stolpci, barva)
 
 def vse_diagonale(tabela):
 	matrika1 = np.array(tabela)
@@ -94,8 +94,8 @@ def vse_diagonale(tabela):
 
 	return diagonale
 
-def vrednost_diagonal(tabela, na_potezi):
-		return vrednost_vrstic(vse_diagonale(tabela), na_potezi)
+def vrednost_diagonal(tabela, barva):
+		return vrednost_vrstic(vse_diagonale(tabela), barva)
 
 def vrednost_skupaj(tabela, barva):
 	if barva == CRNI:

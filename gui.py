79c3f91cc3efaf8d7599a2a1ae1025ef2a134757
bbYYfaks <bbYYJnhA@bitbucked.org>
2016-03-22 10:24:52 +0100

    Dodani ukazi na gumbe, ki še ne delujejo

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-import tkinter
+﻿import tkinter
 from igra import*
 from clovek import*
 
@@ -10,16 +10,25 @@ class Gui():
 		self.napis1 = tkinter.StringVar(master, value="Dobrodošli v 五子棋")
 		tkinter.Label(master, textvariable=self.napis1).grid(row=0, column=0)
 
-		#tukaj bi raje dodal napis self.stanje
-		self.napis2 = tkinter.StringVar(master, value="Igrate 5 v vrsto")
-		tkinter.Label(master, textvariable=self.napis2).grid(row=2, column=0)
+		#tukaj bi raje dodal napis self.napis
+		self.napis2 = tkinter.StringVar(master, value="Na potezi je črni")
+		tkinter.Label(master, textvariable=self.napis2).grid(row=42, column=0)
 
 		self.plosca = tkinter.Canvas(master, width=648, height=648, bg = "green", borderwidth=0)
-		self.plosca.grid(row=1, column=0)
+		self.plosca.grid(row=1, column=0, columnspan=1, rowspan=41)
 		
 		self.gumb1 = tkinter.Button(master, text="Nova igra", command=self.koncaj_igro)
 		self.gumb1.grid(row=1, column=1)
 		# gumn, ki razveljavi potezo ...
+
+		self.gumb2 = tkinter.Button(master, text="Razveljavi", command=self.razveljavi_gumb)
+		self.gumb2.grid(row=2, column=1)
+
+		self.gumb3 = tkinter.Button(master, text="Računalnik črni", command=self.rac_crni)
+		self.gumb3.grid(row=3, column=1)
+
+		self.gumb4 = tkinter.Button(master, text="Računalnik beli", command=self.rac_beli)
+		self.gumb4.grid(row=4, column=1)
 
 		#!!! manjkata dve črti !!!
 		for i in range(18):
@@ -29,8 +38,17 @@ class Gui():
 
 		self.igra = Igra(self)
 		self.igra.na_potezi.igraj()
-	
-	
+
+	def rac_crni(self):
+		self.igra.igralec1 = Racunalnik(self)
+		self.igra.na_potezi.igraj()
+	def rac_beli(self):
+		self.igra.igralec2 = Racunalnik(self)
+		self.igra.na_potezi.igraj()
+	def razveljavi_gumb(self):
+		self.igra.razveljavi
+		self.napis2.set("Razveljavitev gotovo ni grafično podprta")
+
 	def narisi1(self, i, j):
 		x = i * 36
 		y = j * 36
@@ -39,15 +57,29 @@ class Gui():
 		x = i * 36
 		y = j * 36
 		self.plosca.create_oval(x - 18, y - 18, x + 18, y + 18, fill="white")     
+
+	def narisi_crto(self, kje):
+		(y0, x0) = kje[0]
+		(y1, x1) = kje[4]
+		self.plosca.create_line(x0 * 36, y0 * 36, x1 * 36, y1 * 36, fill="red", width="3")
+
+
 	def koncaj_igro(self):
-		print("Konec")
+		aplikacija = Gui(root)
+		self.igra = Igra(self)
+		self.igra.na_potezi.igraj()
+
 	def povleci_potezo(self, i, j):
 		if self.igra.pravilna(i, j):
 			if self.igra.na_potezi == self.igra.igralec1:
 				self.narisi1(i, j)
+				self.napis2.set("Na potezi je beli")
 			else:
 				self.narisi2(i, j)
+				self.napis2.set("Na potezi je črni")
 			self.igra.povleci(i, j)
+		#else:
+		#	self.napis2.set("Igra je končana")
 
 if __name__ == "__main__":
 	# Naredimo glavno okno in nastavimo ime
